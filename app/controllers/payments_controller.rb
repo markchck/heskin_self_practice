42756ea928ac3f21ba9055bbe9d5796b73a663b7
Jaemin Ki <markchck@gmail.com>
2021-08-14 19:45:30 +0900

    ajax방식으로 넘기는 법 코드 추가/ form 방식으로 넘기는건 주석처리

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,27 +1,49 @@
 class PaymentsController < ApplicationController
   def create
-    # response = Iamport.payment(params["imp_uid"])
+    # 1. form방식으로 넘겼을 때 Create액션
+      # order = Order.find(params[:order_id])
+      # if params[:imp_uid].present?
+      #   response = Iamport.payment(params[:imp_uid])
+      #   Payment.create(
+      #     order_id: order,
+      #     response: response,
+      #     imp_uid: response["response"]["imp_uid"],
+      #     merchant_uid: response["response"]["merchant_uid"],
+      #     amount: response["response"]["amount"]
+      #   )
+      #   order.update(status: "processed")
+      #   flash[:notice] = "결제 되었습니다"
+      # else
+      #   error_msg = params[:error_msg]
+      #   order.update(status: "failed")
+      #   flash[:notice] = error_msg
+      # end
+      # redirect_to "/orders"
 
-    order = Order.find(params[:order_id])
-    if params[:imp_uid].present?
-      response = Iamport.payment(params[:imp_uid])
-      Payment.create(
-        order_id: order,
-        response: response,
-        imp_uid: response["response"]["imp_uid"],
-        merchant_uid: response["response"]["merchant_uid"],
-        amount: response["response"]["amount"]
-      )
+    # 2. Ajax로 넘겼을 때 Create 액션
+      order = Order.find(params[:order_id])
+ 
+      if params[:imp_uid].present?
+        response = Iamport.payment(params[:imp_uid])
+        Payment.create(
+          order_id: order,
+          response: response,
+          imp_uid: response["response"]["imp_uid"],
+          merchant_uid: response["response"]["merchant_uid"],
+          amount: response["response"]["amount"]
+        )
+        order.update(status: "processed")
+        @notice = "결제 되었습니다"
 
-      order.update(status: "processed")
-
-      flash[:notice] = "결제 되었습니다"
-    else
-      error_msg = params[:error_msg]
-      order.update(status: "failed")
-      flash[:notice] = error_msg
-    end
-    
-    redirect_to "/orders"
+      else
+        error_msg = params[:error_msg]
+        order.update(status: "failed")
+        @notice = error_msg
+      end
+      
+      respond_to do |format|
+        format.js
+      end
+      
   end
 end
