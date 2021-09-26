445a8080619b051e024289df8966ead093bd1ba9
Jaemin Ki <markchck@gmail.com>
2021-09-26 21:46:54 +0900

    엘라스틱빈스토크 방식

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_08_19_123201) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.bigint "resource_id"
+    t.integer "resource_id"
     t.string "author_type"
-    t.bigint "author_id"
+    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -32,8 +29,8 @@ ActiveRecord::Schema.define(version: 2021_08_19_123201) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -63,8 +60,8 @@ ActiveRecord::Schema.define(version: 2021_08_19_123201) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "pack_id"
+    t.integer "user_id"
+    t.integer "pack_id"
     t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -73,8 +70,8 @@ ActiveRecord::Schema.define(version: 2021_08_19_123201) do
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.bigint "order_id"
-    t.bigint "pack_id"
+    t.integer "order_id"
+    t.integer "pack_id"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -83,7 +80,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_123201) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "user_id"
+    t.integer "user_id"
     t.integer "status", default: 0
     t.string "name"
     t.string "phone"
@@ -107,7 +104,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_123201) do
   end
 
   create_table "payments", force: :cascade do |t|
-    t.bigint "order_id"
+    t.integer "order_id"
     t.json "response"
     t.string "imp_uid"
     t.string "merchant_uid"
@@ -133,11 +130,4 @@ ActiveRecord::Schema.define(version: 2021_08_19_123201) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "carts", "packs"
-  add_foreign_key "carts", "users"
-  add_foreign_key "order_items", "orders"
-  add_foreign_key "order_items", "packs"
-  add_foreign_key "orders", "users"
-  add_foreign_key "payments", "orders"
 end
