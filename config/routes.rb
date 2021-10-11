92ecceffbd31cc958deeb2bda9994deb5088a44b
Jaemin Ki <markchck@gmail.com>
2021-10-11 22:12:38 +0900

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'orders/create'
   get 'orders/show'
   get 'carts/create'
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
